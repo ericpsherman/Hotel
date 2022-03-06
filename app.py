164c6cc92fb060ab0f9b9ef1dfd9ccebc3003619
ericpsherman <ericsherman216@gmail.com>
2022-03-06 22:58:30 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, url_for, render_template, redirect
+from database import data
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return "Hello World!"
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
