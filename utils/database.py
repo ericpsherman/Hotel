d55edb621097e9d6286dcb0f79c0a8966af7832c
ericpsherman <ericsherman216@gmail.com>
2022-03-07 12:55:38 +0530

    Initial database setup

--- utils/database.py
+++ utils/database.py
@@ -1,1 +1,11 @@
 from .database_connection import DatabaseConnection
+
+
+"""
+    
+"""
+def initialize() -> None:
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+
