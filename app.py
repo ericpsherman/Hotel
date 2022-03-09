058c8b03bb93b8ff7bd02602d013e84805627a64
ericpsherman <ericsherman216@gmail.com>
2022-03-09 11:29:33 +0530

    Booking a room functionality (COMPLETE)

--- app.py
+++ app.py
@@ -15,10 +15,15 @@ def booking():
     if request.method == 'POST':
         date_from = request.form.get('date-from')
         date_to = request.form.get('date-to')
+        print(date_from,date_to)
         database.show_available_rooms(date_from, date_to)
     return render_template('booking.html')
 
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    database.is_available('2022-03-09', '2022-03-14', 'A Class')
+    # app.run(debug=True)
+    booking_availability = database.is_available('2022-03-09', '2022-03-14', 'A Class')
+    print(booking_availability)
+    if booking_availability[0]:
+        database.book_a_room(booking_availability[1], booking_availability[2], booking_availability[3])
+    database.display()
